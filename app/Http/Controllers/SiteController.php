2cab2a708aacd59ff0b69b0e0640bb4802338bdd
Mário Lucas <mariolucasdev@gmail.com>
2022-12-06 21:17:42 -0300

    Add Routes to Site

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -15,4 +15,35 @@ class SiteController extends Controller
     {
         echo 'Home page';
     }
+
+    /**
+     * Show About Page
+     *
+     * @return void
+     */
+    public function about()
+    {
+        echo 'About page';
+    }
+
+    /**
+     * Show Services Page
+     *
+     * @return void
+     */
+    public function services()
+    {
+        echo 'Services page';
+    }
+
+    /**
+     * Show Service Page
+     *
+     * @param int $id
+     * @return void
+     */
+    public function service(int $id = null)
+    {
+        echo "Services page id: {$id}";
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [SiteController::class, 'index']);
+Route::get('/sobre', [SiteController::class, 'about']);
+Route::get('/servicos', [SiteController::class, 'services']);
+Route::get('/servico/{id?}', [SiteController::class, 'service']);
