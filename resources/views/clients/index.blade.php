8c770066726e6e748edc61ddcb591fc0e7e98ea2
Mário Lucas <mariolucasdev@gmail.com>
2022-12-08 16:48:31 -0300

    Fix Layout To Not Repeat Code With Template Extension

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -1,48 +1,27 @@
-<!doctype html>
-<html lang="en">
+@extends('app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Lista de Clientes</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-</head>
+@section('titulo', 'Lista de Clientes')
 
-<body>
-    <!-- As a link -->
-    <nav class="navbar bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-        </div>
-    </nav>
-
-
-    <div class="container">
-        <h1> Lista de Clientes </h1>
-        <table class="table">
-            <thead>
+@section('conteudo')
+    <h1> Lista de Clientes </h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Endereço</th>
+                <th scope="col">Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($clients as $client)
                 <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Nome</th>
-                    <th scope="col">Endereço</th>
-                    <th scope="col">Ações</th>
+                    <th scope="row">{{ $client->id }}</th>
+                    <td><a href="{{ route('clients.show', $client) }}">{{ $client->nome }}</a></td>
+                    <td>{{ $client->endereco }}</td>
+                    <td></td>
                 </tr>
-            </thead>
-            <tbody>
-                @foreach($clients as $client)
-                    <tr>
-                        <th scope="row">{{ $client->id }}</th>
-                        <td><a href="{{ route('clients.show', $client) }}">{{ $client->nome }}</a></td>
-                        <td>{{ $client->endereco }}</td>
-                        <td></td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
-</body>
-
-</html>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
--- resources/views/clients/show.blade.php
+++ resources/views/clients/show.blade.php
@@ -1,40 +1,19 @@
-<!doctype html>
-<html lang="en">
+@extends('app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Detalhes do Cliente</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-</head>
+@section('titulo', 'Detalhes de Cliente')
 
-<body>
-    <!-- As a link -->
-    <nav class="navbar bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+@section('conteudo')
+    <div class="card">
+        <div class="card-header">
+            Detalhes do Cliente {{ $client->nome }}
         </div>
-    </nav>
-
-
-    <div class="container">
-        <div class="card">
-            <div class="card-header">
-              Detalhes do Cliente {{ $client->nome }}
-            </div>
-            <div class="card-body">
-                <p> <strong> ID: </strong> {{ $client->id }} </p>
-                <p> <strong> Nome: </strong> {{ $client->nome }} </p>
-                <p> <strong> Endereço: </strong> {{ $client->endereco }} </p> <hr>
-                <p> <strong> Observações: </strong> {{ $client->observacoes }} </p>
-                <br>
-                <a class="btn btn-success" href="{{ route('clients.index') }}"> Voltar </a>
-            </div>
+        <div class="card-body">
+            <p> <strong> ID: </strong> {{ $client->id }} </p>
+            <p> <strong> Nome: </strong> {{ $client->nome }} </p>
+            <p> <strong> Endereço: </strong> {{ $client->endereco }} </p> <hr>
+            <p> <strong> Observações: </strong> {{ $client->observacoes }} </p>
+            <br>
+            <a class="btn btn-success" href="{{ route('clients.index') }}"> Voltar </a>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
-</body>
-
-</html>
+@endsection
